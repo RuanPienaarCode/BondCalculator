277a7812c784da365366db79278e8cfe0f21aa66
Ruan <Ruan3d@gmail.com>
2019-10-30 21:02:56 +0200

    more..

--- src/node.js
+++ src/node.js
@@ -4,7 +4,8 @@ const express = require('express')
 const app = express()
 const port = (process.env.PORT || 3000)
 
-const staticDirPath = path.join(__dirname, '../public/')
+const staticDirPath = path.join(__dirname, '../public')
+
 app.use(express.static(staticDirPath))
 
 app.get('', (req, res) => {
