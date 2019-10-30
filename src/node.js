d7b56e96a2fbf4f419bc04a0a4b2b3738d6f3c54
Ruan <Ruan3d@gmail.com>
2019-10-30 17:12:51 +0200

    Creating app project

---
+++ src/node.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const express = require('express')
+
+
+const app = express()
+const staticDirPath = path.join(__dirname, '../public/')
+
+
+const port = process.env.PORT || 3000
+app.set('view engine', 'hbs')
+app.use(express.static(staticDirPath))
+
+app.get('', (req, res) => {
+
+    res.render('index', {
+        title: 'Morgage Calculator',
+        name: 'Ruan Pienaar'
+    })
+})
+
+app.listen(port, () => {
+    console.log('server is running on port ' + port)
+})
