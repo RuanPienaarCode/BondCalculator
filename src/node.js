028e16eb4b9ce763ac4f2ee6bea1507b8535c78d
Ruan <Ruan3d@gmail.com>
2019-10-30 19:46:59 +0200

    same..

--- src/node.js
+++ src/node.js
@@ -3,12 +3,12 @@ const express = require('express')
 
 
 const app = express()
-const staticDirPath = path.join(__dirname, '../public/')
-
+// const staticDirPath = path.join(__dirname, '../public/')
+var distDir = __dirname + "/dist/";
 
 const port = process.env.PORT || 3000
 // app.set('view engine', 'hbs')
-app.use(express.static(staticDirPath))
+app.use(express.static(distDir))
 
 app.get('/404', (req, res) => {
 
